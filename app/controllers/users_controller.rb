5e2bfaa5e91e9d073c1629bd3d23a5ece0ee3d9c
Apurva <apurvanagar@gmail.com>
2021-01-19 15:53:33 +0530

    Added ability to add a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+    response_text = "New user created with id #{new_user.id}"
+    render plain: response_text
   end
 end
