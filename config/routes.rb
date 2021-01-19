bdee3cdc0fe749c022f8e2b074272ec0c0eff4a7
Apurva <apurvanagar@gmail.com>
2021-01-19 16:13:21 +0530

    Cleaned up comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # get "todos", to: "todos#index"
-  # post "todos", to: "todos#create"
-  # get "todos/:id", to: "todos#show"
   resources :todos
   resources :users
   post "/users/login", to: "users#authenticate"
