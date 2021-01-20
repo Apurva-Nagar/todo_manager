4dee7dad8e044a0bd38ea2660e438dada8869e98
Apurva <apurvanagar@gmail.com>
2021-01-20 20:01:31 +0530

    Rendering todos from DB as HTML using ERB

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
+    render "index"
   end
 
   def show
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,21 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[X]" : "[ ]"
-    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed}"
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
   end
 end
