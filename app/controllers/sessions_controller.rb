991a8e230850b09d90203058f5af17b0b579d340
Apurva <apurvanagar@gmail.com>
2021-01-22 20:17:47 +0530

    Added user signin functionality (without sessions)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+    render "new"
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user &&user.authenticate(params[:password])
+      render plain: "You have entered the correct password."
+    else
+      render plain: "Incorrect password"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get "/", to: "home#index"
   resources :todos
   resources :users
+  get "/signin" => "sessions#new", as: :new_session
+  post "/signin" => "sessions#create", as: :session
   post "/users/login", to: "users#authenticate"
 end
