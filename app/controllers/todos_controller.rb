9d48c21d42eccd2b840cd02aa47e0ccc1305aa28
Apurva <apurvanagar@gmail.com>
2021-01-22 13:35:19 +0530

    Can update todo complete status via checkbox

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -28,6 +28,6 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
-    render plain: "Updated todo completed status to #{completed}"
+    redirect_to todos_path
   end
 end
