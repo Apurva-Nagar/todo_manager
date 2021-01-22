1337758372fd7f98563aafdfa31a0999c8cc748c
Apurva <apurvanagar@gmail.com>
2021-01-22 22:06:45 +0530

    added signing-in, updated sessions and restricted access to protected routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def new
     render "new"
   end
 
   def create
     user = User.find_by(email: params[:email])
-    if user &&user.authenticate(params[:password])
-      render plain: "You have entered the correct password."
+    if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
+      redirect_to todos_path
     else
       render plain: "Incorrect password"
     end
