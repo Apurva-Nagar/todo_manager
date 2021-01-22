24f5095d78a894b52032f8c4f79e2bf998c375ef
Apurva <apurvanagar@gmail.com>
2021-01-22 19:33:29 +0530

    using has_secure_password to store password_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
-      # password: params[:password],
+      password: params[:password],
     )
     redirect_to "/"
   end
