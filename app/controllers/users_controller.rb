0471554a72456fe8d3f8b086e7093194c26e7e62
Apurva <apurvanagar@gmail.com>
2021-01-23 01:44:16 +0530

    used validate to make first_name and email mandatory for sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.create!(
+    user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
     )
-    session[:current_user_id] = user.id
-    redirect_to "/"
+    if user.save
+      session[:current_user_id] = user.id
+      redirect_to "/"
+    else
+      flash[:error] = user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
 
   def authenticate
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :email, presence: true
   has_secure_password
   has_many :todos
 
   def to_pleasant_string
-    "#{id}. | #{name} | #{email}"
+    "#{id}. | #{first_name} | #{email}"
   end
 end
