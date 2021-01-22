a70a0554f9919fd0984819b89743ea32b106b0aa
Apurva <apurvanagar@gmail.com>
2021-01-22 23:03:42 +0530

    added sign-out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,4 +14,10 @@ class SessionsController < ApplicationController
       render plain: "Incorrect password"
     end
   end
+
+  def destroy
+    session[:current_user_id] = nil
+    @current_user = nil
+    redirect_to "/"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users
   get "/signin" => "sessions#new", as: :new_session
   post "/signin" => "sessions#create", as: :session
+  delete "/signout" => "sessions#destroy", as: :destroy_session
   post "/users/login", to: "users#authenticate"
 end
